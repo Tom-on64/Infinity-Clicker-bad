7b805ad6364ea3b5d2445a9aa323aff901dc5c3a
Tom-on64 <kubecekt@gmail.com>
2022-09-04 12:01:53 +0200

    Some Ui Modifications

--- js/index.js
+++ js/index.js
@@ -1,252 +1,133 @@
-// save file
+// SaveFile
 var saveFile = {
-  "power":{
-    "number":0, 
-    "npc":0, 
-    "nps":0
+  power: {
+    number: 0
   }, 
-  "upgrades":{
-      "npc":{
-          "cost":[50, 200, 500], 
-          "amount":[1, 0, 0], 
-          "bought":[1, 0, 0],
-          "mult":[1.3, 1.5, 1.7]
-        }, 
-      "nps": {
-        "cost": [1000, 5000, 10000],
-        "amount": [0, 0, 0],
-        "bought": [0, 0, 0],
-        "mult": [1.5, 1.6, 1.7]
-      }, 
-      "gen": {
-        "cost": [15000, 20000], 
-        "interval": [20000, 20000], 
-        "mult": [1.8, 1.9], 
-        "amount": [0, 0],
-        "isBought": [false, false], 
-        "online": [false, false]
+  upgrades: {
+    npc: {
+      cost: 50, 
+      bought: 0, 
+      amount: 1, 
+      mult: 1.07
+    }, 
+    nps: {
+      cost: 1000,
+      bought: 0,
+      amount: 0,
+      mult: 1.3
+    }
+  }, 
+  settings: {
+    updateRate: 100
+  }
+}
+
+const increment = () => {
+  saveFile.power.number += saveFile.upgrades.npc.amount
+}
+
+const buy = (_item, _amount) => {
+  if (_amount !== 0)
+  switch (_item) {
+    case "npc":
+      if (saveFile.upgrades.npc.cost * _amount <= saveFile.power.number) {
+        saveFile.power.number -= saveFile.upgrades.npc.cost * _amount * saveFile.upgrades.npc.mult
+        saveFile.upgrades.npc.bought += _amount
+        saveFile.upgrades.npc.amount += _amount
+        saveFile.upgrades.npc.cost *= saveFile.upgrades.npc.mult
       }
-   }, 
-   "achivement": {
-    "normal": [0, 0, 0, 0]
-   }
-}
-// setup
-var hasPlayed = false
+      break
+    case "nps":
+      if (saveFile.upgrades.nps.cost * _amount <= saveFile.power.number) {
+        saveFile.power.number -= saveFile.upgrades.nps.cost * _amount * saveFile.upgrades.nps.mult
+        saveFile.upgrades.nps.bought += _amount
+        saveFile.upgrades.nps.amount += _amount
+        saveFile.upgrades.nps.cost *= saveFile.upgrades.nps.mult
+      }
+      break
 
-function increment () {
-  saveFile.power.number += saveFile.power.npc
-}
-
-// Format
-function format(amount) {
-    if (amount < 1000) {
-        return amount.toFixed(2)
-    }
-    else if (amount < 1000000 ) {
-        let divided = amount / 1000
-        return divided.toFixed(2) + "K"
-    }
-    else if (amount < 1000000000){
-        let divided = amount / 1000000
-        return divided.toFixed(2) + "M"
-    }
-    else if (amount < 1000000000000) {
-        let divided = amount / 1000000000
-        return divided.toFixed(2) + "B"
-    }
-    else if (amount < 1000000000000000) {
-        let divided = amount / 1000000000000
-        return divided.toFixed(2) + "T"
-    }
-    else if (amount < 1000000000000000000) {
-        let divided = amount / 1000000000000000
-        return divided.toFixed(2) + "Qa"
-    }
-    else if (amount < 1000000000000000000000) {
-        let divided = amount / 1000000000000000000
-        return divided.toFixed(2) + "Qt"
-    }
-    else if (amount < 1000000000000000000000000) {
-        let divided = amount / 1000000000000000000000
-        return divided.toFixed(2) + "Sx"
-    }
-    else if (amount < 1000000000000000000000000000) {
-        let divided = amount / 1000000000000000000000000
-        return divided.toFixed(2) + "Sp"
-    }
-    else if (amount < 1000000000000000000000000000000) {
-        let divided = amount / 1000000000000000000000000000
-        return divided.toFixed(2) + "Oc"
-    }
-    else if (amount < 1000000000000000000000000000000000) {
-        let divided = amount / 1000000000000000000000000000000
-        return divided.toFixed(2) + "No"
-    }
-    else if (amount < 1000000000000000000000000000000000000) {
-        let divided = amount / 1000000000000000000000000000000000
-        return divided.toFixed(2) + "Dc"
-    }
-    else {
-        let power = Math.floor(Math.log10(amount))
-        let mantissa = amount / Math.pow(10, power)
-        return mantissa.toFixed(2) + "e" + power
-    }
-}
-
-function buyUpgrade (type) {
-  if (type == "npc1") {
-    let s = saveFile.upgrades.npc
-    if (s.cost[0] <= saveFile.power.number) {
-      saveFile.power.number -= s.cost[0]
-      saveFile.power.npc += 1
-      s.cost[0] = s.cost[0] * s.mult[0]
-      s.bought += 1
-      s.amount += 1
-      document.getElementById("npc1").innerHTML = "NPC +1<br><br>Cost: " + format(s.cost[0])
-    }
-  }
-  else if (type == "npc2") {
-    let s = saveFile.upgrades.npc
-    if (s.cost[1] <= saveFile.power.number) {
-      saveFile.power.number -= s.cost[1]
-      saveFile.power.npc += 5
-      s.cost[1] = s.cost[1] * s.mult[1]
-      s.bought += 1
-      s.amount += 1
-      document.getElementById("npc2").innerHTML = "NPC +5<br><br>Cost: " + format(s.cost[1])
-    }
-  }
-  else if (type == "npc3") {
-    let s = saveFile.upgrades.npc
-    if (s.cost[2] <= saveFile.power.number) {
-      saveFile.power.number -= s.cost[2]
-      saveFile.power.npc += 10
-      s.cost[2] = s.cost[2] * s.mult[2]
-      s.bought += 1
-      s.amount += 1
-      document.getElementById("npc3").innerHTML = "NPC +10<br><br>Cost: " + format(s.cost[2])
-    }
-  }
-  if (type == "nps1") {
-    let s = saveFile.upgrades.nps
-    if (s.cost[0] <= saveFile.power.number) {
-      saveFile.power.number -= s.cost[0]
-      saveFile.power.nps += 1
-      s.cost[0] = s.cost[0] * s.mult[0]
-      s.bought += 1
-      s.amount += 1
-      document.getElementById("nps1").innerHTML = "NPS +1<br><br>Cost: " + format(s.cost[0])
-    }
-  }
-  else if (type == "nps2") {
-    let s = saveFile.upgrades.nps
-    if (s.cost[1] <= saveFile.power.number) {
-      saveFile.power.number -= s.cost[1]
-      saveFile.power.nps += 5
-      s.cost[1] = s.cost[1] * s.mult[1]
-      s.bought += 1
-      s.amount += 1
-      document.getElementById("nps2").innerHTML = "NPS +5<br><br>Cost: " + format(s.cost[1])
-    }
-  }
-  else if (type == "nps3") {
-    let s = saveFile.upgrades.nps
-    if (s.cost[2] <= saveFile.power.number) {
-      saveFile.power.number -= s.cost[2]
-      saveFile.power.nps += 10
-      s.cost[2] = s.cost[2] * s.mult[2]
-      s.bought += 1
-      s.amount += 1
-      document.getElementById("nps3").innerHTML = "NPS +10<br><br>Cost: " + format(s.cost[2])
-    }
-  }
-  if (type == "gen1") {
-    let s = saveFile.upgrades.gen
-    if (s.cost[0] <= saveFile.power.number) {
-      saveFile.power.number -= s.cost[0]
-      s.cost[0] = s.cost[0] * s.mult[0]
-      s.isBought = true
-      s.amount += 1
-      document.getElementById("gen1").innerHTML = "NPC Generator<br><button onClick='if (saveFile.upgrades.gen.online[0] = true) saveFile.upgrades.gen.online[0] = false; else saveFile.upgrades.gen.online[0] = true'>Online</button><br><br><button>Upgrade<br>Cost: " + format(s.cost[0]) + "</button>"
-    }
+    default:
+      console.log("Invalid Item To Buy!")
+      break
   }
 }
 
-function changeTab (tab) {
-  document.getElementById("infinityPage").style.display = "none"
-  document.getElementById("infinitumPage").style.display = "none"
-  document.getElementById("achivementPage").style.display = "none"
-  document.getElementById("helpPage").style.display = "none"
-  document.getElementById("aboutPage").style.display = "none"
-  document.getElementById("devPage").style.display = "none"
-  document.getElementById("startPage").style.display = "none"
-
-  page = document.getElementById(tab + "Page").style.display = "block"
+const format = (_number) => {
+  if (_number == undefined) {
+    return '0'
+  }
+  else if (_number < 1000) {
+    return _number.toFixed(2)
+  }
+  else if (_number < 1000000) {
+    let divided = _number / 1000
+    return divided.toFixed(2) + "K"
+  }
+  else if (_number < 1000000000) {
+    let divided = _number / 1000000
+    return divided.toFixed(2) + "M"
+  }
+  else if (_number < 1000000000000) {
+    let divided = _number / 1000000000
+    return divided.toFixed(2) + "B"
+  }
+  else if (_number < 1000000000000000) {
+    let divided = _number / 1000000000000
+    return divided.toFixed(2) + "T"
+  }
+  else if (_number < 1000000000000000000) {
+    let divided = _number / 1000000000000000
+    return divided.toFixed(2) + "Qa"
+  }
+  else if (_number < 1000000000000000000000) {
+    let divided = _number / 1000000000000000000
+    return divided.toFixed(2) + "Qt"
+  }
+  else if (_number < 1000000000000000000000000) {
+    let divided = _number / 1000000000000000000000
+    return divided.toFixed(2) + "Sx"
+  }
+  else if (_number < 1000000000000000000000000000) {
+    let divided = _number / 1000000000000000000000000
+    return divided.toFixed(2) + "Sp"
+  }
+  else if (_number < 1000000000000000000000000000000) {
+    let divided = _number / 1000000000000000000000000000
+    return divided.toFixed(2) + "Oc"
+  }
+  else if (_number < 1000000000000000000000000000000000) {
+    let divided = _number / 1000000000000000000000000000000
+    return divided.toFixed(2) + "No"
+  }
+  else if (_number < 1000000000000000000000000000000000000) {
+    let divided = _number / 1000000000000000000000000000000000
+    return divided.toFixed(2) + "Dc"
+  }
+  else {
+    let power = Math.floor(Math.log10(_number))
+    let mantissa = _number / Math.pow(10, power)
+    return mantissa.toFixed(2) + "e" + power
+  }
 }
 
-function alert(message, textColor="white", bgColor="none", timeout=10000) {
-  document.getElementById("alertMessage").textContent = message
-  document.getElementById("alertMessage").style.backgroundColor = bgColor
-  document.getElementById("alertMessage").style.color = textColor
-
-  setTimeout(function () {
-    document.getElementById("alertMessage").textContent = ""
-    document.getElementById("alertMessage").style.backgroundColor = "none"
-    document.getElementById("alertMessage").style.color = "none"
-  }, timeout)
-
-  
-}
-
-function save () {
-  localStorage.setItem("savedFile", JSON.stringify(saveFile))
-}
-
-function load () {
-  var saveFile = JSON.parse(localStorage.getItem("savedFile"))
-}
-
-function productionLoop () {
-  saveFile.power.number += saveFile.power.nps / 20
-
-
-}
-
-function render () {
-  const numberText = document.getElementById("numberText")
-  const numberTextBottom = document.getElementById("numberTextBottom")
-  const npcText = document.getElementById("npcText")
-  const npsText = document.getElementById("npsText")
-  const title = document.getElementById("webTitle")
-  
-  title.textContent = "Infinity Clicker - " + format(saveFile.power.number)
-  numberText.textContent = format(saveFile.power.number)
-  numberTextBottom.textContent = format(saveFile.power.number)
-  npcText.textContent = format(saveFile.power.npc)
-  npsText.textContent = format(saveFile.power.nps)
-
-  /* for (let i = 1; i < 3; i += 1) {
-    let index = i
-    if (saveFile.achivement.normal[i - 1] == 1) document.getElementById("a" + index).classList.add = "active"
-    else document.getElementById("a" + index).classList.remove = "active"
-  } */
-
-  checkLoop()
-        
-  requestAnimationFrame(render)
-}
-
-function checkLoop () {
-let p = saveFile.power
-
-  // Achivements
-  let a = saveFile.achivement.normal
-  if ( p.npc >= 1  && a[0] < 1) { a[0] = 1; alert("Achivement! It's A Start!", "yellow", "blue") }
-  if ( p.npc >= 10 && a[1] < 1) { a[1] = 1; alert("Achivement! It 'Aint Much, But It's Honest Work.", "yellow", "blue") }
-  if ( p.nps >= 1  && a[2] < 1) { a[2] = 1; alert("Achivement! Stable Income", "yellow", "blue") }
-  if ( saveFile.upgrades.gen.amount[0] == 1) { saveFile.upgrades.gen.amount[0] = 2; alert("Achivement! A New GENeration", "yellow", "blue") }
-}
-
-setInterval(productionLoop, 50)
-render()
+setInterval(function () {
+  // Render
+  document.getElementById("webTitle").textContent = `Infinity Clicker | ${format(saveFile.power.number)}`
+  document.getElementById("npcText").textContent = format(saveFile.upgrades.npc.amount)
+  document.getElementById("numberText").textContent = format(saveFile.power.number)
+  document.getElementById("npsText").textContent = format(saveFile.upgrades.nps.amount)
+  document.getElementById("npc1cost").textContent = format(saveFile.upgrades.npc.cost * 1)
+  document.getElementById("npc5cost").textContent = format(saveFile.upgrades.npc.cost * 5)
+  document.getElementById("npc10cost").textContent = format(saveFile.upgrades.npc.cost * 10)
+  document.getElementById("npc100cost").textContent = format(saveFile.upgrades.npc.cost * 100)
+  document.getElementById("nps1cost").textContent = format(saveFile.upgrades.nps.cost * 1)
+  document.getElementById("nps5cost").textContent = format(saveFile.upgrades.nps.cost * 5)
+  document.getElementById("nps10cost").textContent = format(saveFile.upgrades.nps.cost * 10)
+  document.getElementById("nps100cost").textContent = format(saveFile.upgrades.nps.cost * 100)
+  if (saveFile.upgrades.nps.amount !== 0) document.getElementById("npsContainer").style.display = "block"
+  else document.getElementById("npsContainer").style.display = "none"
+}, saveFile.settings.updateRate)
+setInterval(function () {
+  // Math
+  saveFile.power.number += saveFile.upgrades.nps.amount / 20
+}, 50)
